fad813094c69f3c1170515a4220535513b13451a
sagaragarwal94 <atomsagar@gmail.com>
2015-04-14 00:38:07 +0530

    beta stage

--- src/Program.java
+++ src/Program.java
@@ -5,9 +5,10 @@ public class Program {
 
 	
 	public static void main(String[] args) throws Exception {
-		GUI Page1 = new GUI();
+		//GUI Page1 = new GUI();
 		//GUI_AfterLogin page2 = new GUI_AfterLogin();
 		//GUI_Questions Page5 = new GUI_Questions();
+		GUI_Questions page6 = new GUI_Questions("Questions");
 	}
 
 }
